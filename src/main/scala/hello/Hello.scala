fec0a4b1a932e1e5b66c00fa84e07d21435a9bbe
SimonHGR <simon@dancingcloudservices.com>
2018-02-05 10:19:33 -0500

    Initial commit, hello world

---
+++ src/main/scala/hello/Hello.scala
@@ -0,0 +1,25 @@
+package hello
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala world!")
+  }
+}
+
+
+//package hello {
+//
+//  class Hello {
+//
+//    import scala.io.StdIn
+//  }
+//
+//}
+//
+//package goodbye {
+//
+//  class Goodbye {
+//
+//  }
+//
+//}
