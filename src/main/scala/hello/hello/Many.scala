c58241c9dcd42aec6641a81a78d1b7d3c304db86
SimonHGR <simon@dancingcloudservices.com>
2018-02-05 13:13:30 -0500

    Completed variable length argument list factory form for HasAFactory object

--- src/main/scala/hello/hello/Many.scala
+++ src/main/scala/hello/hello/Many.scala
@@ -3,9 +3,19 @@ package hello.hello
 object HasAFactory {
   def apply(a: Int *): Array[HasAFactory] = {
     for (x <- a) println(s"got an arg $x")
+    val result = new Array[HasAFactory](a.size)
+    var idx = 0;
+    // BAD FORM!?!? Functional style tends to avoid imperative loops, particularly with vars
+    while (idx < a.size) {
+      result(idx) = new HasAFactory(a(idx))
+      idx += 1
+    }
+    result
   }
+
 //  def apply(a: Int, b: Int, c: Int): Array[HasAFactory] =
 //    Array[HasAFactory](new HasAFactory(a), new HasAFactory(b), new HasAFactory(c))
+
 }
 
 class HasAFactory(val x: Int) {
@@ -26,7 +36,7 @@ object Many {
     println(haf)
 
     val several = HasAFactory(1, 9, 99)
-//    for (h <- several) println(h)
+    for (h <- several) println(h)
 
   }
 }
